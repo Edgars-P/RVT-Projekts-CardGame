148701fd8b8fe7f2b56265aa854d9ea29606e326
Edgars Polis <edgars@edgara.id.lv>
2023-11-20 03:04:27 +0200

    Update

--- shell.nix
+++ shell.nix
@@ -2,11 +2,43 @@
 
 let
 
+	# get pocketbase from github release archive
+	newPocketbase = pkgs.buildGoModule rec {
+		name = "pocketbase";
+		version = "v0.20.0-rc3";
+
+		src = pkgs.fetchFromGitHub {
+			owner = "pocketbase";
+			repo = "pocketbase";
+			rev = "v${version}";
+			sha256 = "sha256-9LIOBfNOa+u7yLL7iWb/e7c8ZSiyjukqaY0ifVR2iSs=";
+		};
+
+		vendorHash = "sha256-LFIJClPByaLXtsBOk7SjpJlIuQhWbVIs6H4PXhd7oyo=";
+
+		# This is the released subpackage from upstream repo
+		subPackages = [ "examples/base" ];
+
+		CGO_ENABLED = 0;
+
+		# Upstream build instructions
+		ldflags = [
+			"-s"
+			"-w"
+			"-X github.com/pocketbase/pocketbase.Version=${version}"
+		];
+
+		postInstall = ''
+			mv $out/bin/base $out/bin/pocketbase
+		'';
+	};
+
+
 in pkgs.mkShell {
   packages = with pkgs; [
     nodejs_18
 		nodePackages.pnpm
-		pocketbase
+		newPocketbase
   ];
 
 }
